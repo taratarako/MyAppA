b7536b5443bbd157aad5f7be124f968ab5bb045f
柴田泰良 <185112097+taratarako@users.noreply.github.com>
2025-09-03 14:38:18 +0900

    データを渡すフィールドとボタンの見た目を作成

--- app/src/main/java/jp/ac/meijou/android/s241205089/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205089/MainActivity2.java
@@ -1,5 +1,7 @@
 package jp.ac.meijou.android.s241205089;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +10,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity2 extends AppCompatActivity {
+import jp.ac.meijou.android.s241205089.databinding.ActivityMain2Binding;
+import jp.ac.meijou.android.s241205089.databinding.ActivityMain3Binding;
+import jp.ac.meijou.android.s241205089.databinding.ActivityMainBinding;
 
+public class MainActivity2 extends AppCompatActivity {
+    private ActivityMain2Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //setContentView(R.layout.activity_main2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        //明示的
+        binding.button2.setOnClickListener(view -> {
+            var intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        });
+        //暗黙的
+        binding.button3.setOnClickListener(view -> {
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);;
+            intent.setData(Uri.parse("https://www.yahoo.co.jp"));
+            startActivity(intent);
         });
     }
 }
