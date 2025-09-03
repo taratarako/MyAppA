d09883a77a62c54b8310f01e5aa24fba5f33d264
柴田泰良 <185112097+taratarako@users.noreply.github.com>
2025-09-03 13:33:33 +0900

    ボタンの作り方と電卓のページを作成

---
+++ app/src/main/java/jp/ac/meijou/android/s241205089/MainActivity3.java
@@ -0,0 +1,27 @@
+package jp.ac.meijou.android.s241205089;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import jp.ac.meijou.android.s241205089.databinding.ActivityMain3Binding;
+import jp.ac.meijou.android.s241205089.databinding.ActivityMainBinding;
+
+public class MainActivity3 extends AppCompatActivity {
+    private ActivityMain3Binding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main3);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
